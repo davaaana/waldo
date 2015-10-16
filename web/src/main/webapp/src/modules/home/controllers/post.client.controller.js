cb60f1a7e1e8b930bcbc4f1349b64ec73fda9f1e
davaaana <davaana_bit@yahoo.com>
2015-10-16 12:57:55 -0800

    new project

--- web/src/main/webapp/src/modules/home/controllers/post.client.controller.js
+++ web/src/main/webapp/src/modules/home/controllers/post.client.controller.js
@@ -1,14 +1,71 @@
 'use strict';
 
-mainApp.controller('PostController', ['$rootScope', '$scope','$http','$mdDialog','PostService', 'UserService','$location',
-    function ($rootScope, $scope,$http,dialog,PostService,AuthService,$location) {
+mainApp.controller('PostController', ['$rootScope', '$scope','$http','$mdDialog','PostService','$log','$q', 'UserService','$location',
+    function ($rootScope, $scope,$http,dialog,PostService,$log,$q,AuthService,$location) {
         $scope.filter = {
             froomDate:'',
             tooDate:''
         };
+
+        $scope.simulateQuery = false;
+        $scope.isDisabled    = false;
+        $scope.auth = AuthService.getAuthentication();
+
         $scope.ownBool = false;
         $scope.page = 1;
 
+        $scope.searchTextChange = function(text) {
+            $log.info('Text changed to ' + text);
+        }
+
+        $scope.querySearch = function (query) {
+            var results = query ? $scope.state.filter( $scope.createFilterFor(query) ) : $scope.states,
+                deferred;
+            if ($scope.simulateQuery) {
+                deferred = $q.defer();
+                setTimeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+                return deferred.promise;
+            } else {
+                return results;
+            }
+        }
+
+        $scope.selectedItemChange = function(item) {
+            $log.info('Item changed to ' + JSON.stringify(item));
+        }
+
+        /**
+         * Build `states` list of key/value pairs
+         */
+        $scope.states = function() {
+            var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
+              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
+              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
+              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
+              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
+              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
+              Wisconsin, Wyoming';
+
+            return allStates.split(/, +/g).map( function (state) {
+                return {
+                    value: state.toLowerCase(),
+                    display: state
+                };
+            });
+        }
+
+        $scope.state = $scope.states();
+        /**
+         * Create filter function for a query string
+         */
+        $scope.createFilterFor = function(query) {
+            var lowercaseQuery = angular.lowercase(query);
+
+            return function filterFn(state) {
+                return (state.value.indexOf(lowercaseQuery) === 0);
+            };
+
+        }
 
         PostService.getAllPostData().then(function(posts){
             $scope.posts = posts.data;
