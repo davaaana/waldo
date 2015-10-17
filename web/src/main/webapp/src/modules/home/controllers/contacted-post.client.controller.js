ed75f5ef470db7a2c7cd89f69be4957bc958da37
davaaana <davaana_bit@yahoo.com>
2015-10-17 13:27:40 -0800

    new project

--- web/src/main/webapp/src/modules/home/controllers/contacted-post.client.controller.js
+++ web/src/main/webapp/src/modules/home/controllers/contacted-post.client.controller.js
@@ -1,13 +1,16 @@
 'use strict';
 
-mainApp.controller('ContactedPostController', ['$rootScope', '$scope', '$http', '$mdDialog', 'PostService', '$log', '$q','$mdToast', 'UserService',
-    function ($rootScope, $scope, $http, dialog, PostService, $log, $q,$mdToast, AuthService) {
+mainApp.controller('ContactedPostController', ['$rootScope', '$scope', '$http', '$mdDialog', 'PostService', '$log', '$q', '$mdToast', 'UserService',
+    function ($rootScope, $scope, $http, dialog, PostService, $log, $q, $mdToast, AuthService) {
 
         $scope.auth = AuthService.getAuthentication();
         $scope.contactedNextBtn = true;
+        $scope.rateStep = 1;
 
         $scope.paging = 0;
-        if(!$scope.auth) {window.location.href = '/';}
+        if (!$scope.auth) {
+            window.location.href = '/';
+        }
 
         $scope.contactedPost = function () {
             PostService.getContactedPostList($scope.paging).then(function (data) {
@@ -26,6 +29,16 @@ mainApp.controller('ContactedPostController', ['$rootScope', '$scope', '$http', 
                     $scope.contactedNextBtn = false;
                     $mdToast.showSimple('Цааш мэдээлэл байхгүй байна');
                 }
+            });
+        };
+
+        $scope.contactedPostReadMore = function (contactedPosts) {
+            $scope.contact = contactedPosts;
+            dialog.show({
+                parent: angular.element(document.body),
+                scope: $scope.$new(),
+                controller: 'ContactedPostController',
+                templateUrl: 'src/modules/dialogs/contacted-post-more.client.view.html'
             });
         };
 
